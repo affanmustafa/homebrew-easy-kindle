252bcbdfc50c5aced06b89464445d408845ff2d1
Affan Mustafa <affanmustafa2297@gmail.com>
2025-10-28 20:10:40 +0500

    fix versioning

--- easy-kindle.rb
+++ easy-kindle.rb
@@ -4,7 +4,7 @@
 class EasyKindle < Formula
   desc "CLI tool to convert web articles into EPUB and send to Kindle"
   homepage "https://github.com/affanmustafa/easy-kindle"
-  version "1.0.0"
+  version "1.0"
   
   url "https://github.com/affanmustafa/easy-kindle/releases/download/v#{version}/easy-kindle"
   sha256 "008d9bfcfe88a93d4f821f960c3713b2e45521fdddb4e92f071906356cbefa3a"
