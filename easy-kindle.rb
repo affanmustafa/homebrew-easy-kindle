4d2e1e9c8f22280231a791dadfe5cd35c2f064eb
Affan Mustafa <affanmustafa2297@gmail.com>
2025-11-04 18:45:51 +0500

    v1.3

--- easy-kindle.rb
+++ easy-kindle.rb
@@ -4,10 +4,10 @@
 class EasyKindle < Formula
   desc "CLI tool to convert web articles into EPUB and send to Kindle"
   homepage "https://github.com/affanmustafa/easy-kindle"
-  version "1.2"
+  version "1.3"
   
   url "https://github.com/affanmustafa/easy-kindle/releases/download/v#{version}/easy-kindle"
-  sha256 "6d05d55890c5cfb2c755fea94d81b50d8ad9a3d75e302a63d19f8767b0b31a10"
+  sha256 "7d5b00cb96e0b502501b27b3281ce6a4a972ec2aa99d2041278060992741ef09"
 
   def install
     bin.install "easy-kindle"
